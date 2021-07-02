bdbecec058636e56b99786c4759adbe7f81757fd
Shakya Majumdar <shakyathegreat.majumdar@gmail.com>
2021-07-02 15:33:44 +0530

    refactor: lint youtube.py

--- src/youtube.py
+++ src/youtube.py
@@ -1,119 +1,212 @@
-from youtube_search import YoutubeSearch as ytsearch
 from datetime import datetime, timedelta
-from src.utils import Log, ErrorHandler
-from src.loadingmessage import get_loading_message
+
+import asyncio
+import discord
+import os
+import requests
+import yaml
+from discord.ext import commands
 from pytube import YouTube as YoutubeDownload
-import discord, asyncio, random, os, yaml, requests
+from youtube_search import YoutubeSearch as YTSearch
 
-class YoutubeSearch:   
+from src.loadingmessage import get_loading_message
+from src.utils import ErrorHandler
+
+
+class YoutubeSearch:
     @staticmethod
-    async def search(bot, ctx, message, searchQuery, userSettings): 
-        def resultEmbed(result):
-            embed=discord.Embed(title=result['title'], 
-                description=result['long_desc'] if 'long_desc' in result.keys() else 'No Description', 
-                url=f"https://www.youtube.com{result['url_suffix']}")                  
-            embed.add_field(name="Channel", value=result['channel'], inline=True)
-            embed.add_field(name="Duration", value=result['duration'], inline=True)
-            embed.add_field(name="Views", value=result['views'], inline=True)
-            embed.add_field(name="Publish Time", value=result['publish_time'], inline=True)
+    async def search(
+        bot: commands.Bot,
+        ctx: commands.Context,
+        message: discord.Message,
+        search_query: str,
+        user_settings: dict,
+    ):
+        def result_embed(result_) -> discord.Embed:
+            embed_ = discord.Embed(
+                title=result_["title"],
+                description=result_["long_desc"]
+                if "long_desc" in result_.keys()
+                else "No Description",
+                url=f"https://www.youtube.com{result_['url_suffix']}",
+            )
+            embed_.add_field(name="Channel", value=result_["channel"], inline=True)
+            embed_.add_field(name="Duration", value=result_["duration"], inline=True)
+            embed_.add_field(name="Views", value=result_["views"], inline=True)
+            embed_.add_field(
+                name="Publish Time", value=result_["publish_time"], inline=True
+            )
 
-            embed.set_thumbnail(url=result['thumbnails'][0])
-            embed.set_footer(text=f"Requested by {ctx.author}")
-            return embed
-        
+            embed_.set_thumbnail(url=result_["thumbnails"][0])
+            embed_.set_footer(text=f"Requested by {ctx.author}")
+            return embed_
+
         try:
-            result = ytsearch(searchQuery, max_results=10).to_dict()
+            result = YTSearch(search_query, max_results=10).to_dict()
 
-            embeds = list(map(resultEmbed, result))
+            embeds = list(map(result_embed, result))
 
-            doExit, curPage = False, 0
-            await message.add_reaction('🗑️')
-            await message.add_reaction('⬇️')
+            do_exit, cur_page = False, 0
+            await message.add_reaction("🗑️")
+            await message.add_reaction("⬇️")
             if len(embeds) > 1:
-                await message.add_reaction('◀️')
-                await message.add_reaction('▶️')
+                await message.add_reaction("◀️")
+                await message.add_reaction("▶️")
             elif len(embeds) == 0:
-                embed=discord.Embed(description=f'No results found for: {searchQuery}')
+                embed = discord.Embed(
+                    description=f"No results found for: {search_query}"
+                )
                 await message.edit(content=None, embed=embed)
                 await asyncio.sleep(60)
                 await message.delete()
                 return
-            while doExit == False:
+            while not do_exit:
                 try:
-                    await message.edit(content=None, embed=embeds[curPage%len(embeds)])
-                    reaction, user = await bot.wait_for("reaction_add", check=lambda reaction, user: all([str(reaction.emoji) in ["◀️", "▶️", "🗑️", "⬇️"], reaction.message == message, not user.bot]), timeout=60)
+                    await message.edit(
+                        content=None, embed=embeds[cur_page % len(embeds)]
+                    )
+                    reaction, user = await bot.wait_for(
+                        "reaction_add",
+                        check=lambda reaction_, user_: all(
+                            [
+                                str(reaction_.emoji) in ["◀️", "▶️", "🗑️", "⬇️"],
+                                reaction_.message == message,
+                                not user_.bot,
+                            ]
+                        ),
+                        timeout=60,
+                    )
                     await message.remove_reaction(reaction, user)
-                    if str(reaction.emoji) == '🗑️':
+                    if str(reaction.emoji) == "🗑️":
                         await message.delete()
-                        doExit = True
-                    elif str(reaction.emoji) == '◀️':
-                        curPage-=1
-                    elif str(reaction.emoji) == '▶️':
-                        curPage+=1
-                    elif str(reaction.emoji) == '⬇️' and userSettings[user.id]["downloadquota"]["dailyDownload"] < 50:
+                        do_exit = True
+                    elif str(reaction.emoji) == "◀️":
+                        cur_page -= 1
+                    elif str(reaction.emoji) == "▶️":
+                        cur_page += 1
+                    elif (
+                        str(reaction.emoji) == "⬇️"
+                        and user_settings[user.id]["downloadquota"]["dailyDownload"]
+                        < 50
+                    ):
                         await message.remove_reaction(reaction, bot.user)
-                        downloadmessage = await ctx.send(f'{get_loading_message()} <a:loading:829119343580545074>')
-                        yt = YoutubeDownload(embeds[curPage].url)
-                        download = yt.streams.filter(res='360p', file_extension='mp4', progressive=True).fmt_streams[0]
-                        if round(download.filesize_approx/1000000, 2) < 100:
+                        downloadmessage = await ctx.send(
+                            f"{get_loading_message()} <a:loading:829119343580545074>"
+                        )
+                        yt = YoutubeDownload(embeds[cur_page].url)
+                        download = yt.streams.filter(
+                            res="360p", file_extension="mp4", progressive=True
+                        ).fmt_streams[0]
+                        if round(download.filesize_approx / 1000000, 2) < 100:
                             embed = discord.Embed(
-                                description=f"""{user}, This download will use {round(download.filesize_approx/1000000, 2)}MB of your remaining {50-userSettings[user.id]["downloadquota"]["dailyDownload"]}MB daily quota
-                                                Do you want to continue?""")
+                                description=(
+                                    f"{user}, "
+                                    f"This download will use {round(download.filesize_approx/1000000, 2)}MB "
+                                    f"of your remaining "
+                                    f"{50 - user_settings[user.id]['downloadquota']['dailyDownload']}MB daily quota\n"
+                                    "Do you want to continue?"
+                                )
+                            )
                             await downloadmessage.edit(content=None, embed=embed)
-                            await downloadmessage.add_reaction('👍')
-                            await downloadmessage.add_reaction('👎')
-                            dlreaction, dluser = await bot.wait_for("reaction_add", check=lambda dlreaction, dluser: all([dluser == user, str(dlreaction.emoji) in ['👍', '👎'], dlreaction.message == downloadmessage]), timeout=30)
+                            await downloadmessage.add_reaction("👍")
+                            await downloadmessage.add_reaction("👎")
+                            dlreaction, dluser = await bot.wait_for(
+                                "reaction_add",
+                                check=lambda dlreaction_, dluser_: all(
+                                    [
+                                        dluser_ == user,
+                                        str(dlreaction_.emoji) in ["👍", "👎"],
+                                        dlreaction_.message == downloadmessage,
+                                    ]
+                                ),
+                                timeout=30,
+                            )
 
-                            if str(dlreaction.emoji) == '👍':
+                            if str(dlreaction.emoji) == "👍":
                                 await downloadmessage.clear_reactions()
-                                await downloadmessage.edit(content=f'{get_loading_message()} <a:loading:829119343580545074>', embed=None)
-                                
-                                userSettings[user.id]['downloadquota']['dailyDownload'] += round(download.filesize_approx/1000000, 2)
-                                userSettings[user.id]['downloadquota']['lifetimeDownload'] += round(download.filesize_approx/1000000, 2)
-                                download.download(output_path='./src/cache')
-                                
-                                bestServer = requests.get(url='https://api.gofile.io/getServer').json()['data']['server']
+                                await downloadmessage.edit(
+                                    content=f"{get_loading_message()} <a:loading:829119343580545074>",
+                                    embed=None,
+                                )
 
-                                with open(f'{os.path.abspath(f"./src/cache/{download.default_filename}")}', 'rb') as f:
-                                    url = f'https://{bestServer}.gofile.io/uploadFile'
-                                    params = {'expire':round(datetime.timestamp(datetime.now()+timedelta(minutes=10)))}
-                                    shareLink = requests.post(url=url, params=params, files={f'@{os.path.abspath(f"./src/cache/{download.default_filename}")}': f}).json()['data']['downloadPage']
-                                
+                                user_settings[user.id]["downloadquota"][
+                                    "dailyDownload"
+                                ] += round(download.filesize_approx / 1000000, 2)
+                                user_settings[user.id]["downloadquota"][
+                                    "lifetimeDownload"
+                                ] += round(download.filesize_approx / 1000000, 2)
+                                download.download(output_path="./src/cache")
+
+                                best_server = requests.get(
+                                    url="https://api.gofile.io/getServer"
+                                ).json()["data"]["server"]
+
+                                with open(
+                                    f'{os.path.abspath(f"./src/cache/{download.default_filename}")}',
+                                    "rb",
+                                ) as f:
+                                    url = f"https://{best_server}.gofile.io/uploadFile"
+                                    params = {
+                                        "expire": round(
+                                            datetime.timestamp(
+                                                datetime.now() + timedelta(minutes=10)
+                                            )
+                                        )
+                                    }
+                                    share_link = requests.post(
+                                        url=url,
+                                        params=params,
+                                        files={
+                                            f'@{os.path.abspath(f"./src/cache/{download.default_filename}")}': f
+                                        },
+                                    ).json()["data"]["downloadPage"]
+
                                 os.remove(f"./src/cache/{download.default_filename}")
                                 embed = discord.Embed(
-                                    description=f"""{shareLink}\n\nHere is your download link.
-                                    You now have {50-userSettings[user.id]['downloadquota']['dailyDownload']}MB left in your daily quota. 
-                                    Negative values mean your daily quota for the next day will be subtracted."""
+                                    description=(
+                                        f"{share_link}\n\nHere is your download link. "
+                                        "You now have "
+                                        f"{50 - user_settings[user.id]['downloadquota']['dailyDownload']}MB "
+                                        f"left in your daily quota."
+                                        "Negative values mean your daily quota for the next day will be subtracted."
+                                    )
                                 )
-                                embed.set_footer(text=f'Requested by {user}')
+                                embed.set_footer(text=f"Requested by {user}")
                                 await downloadmessage.delete()
-                                await ctx.send(embed=embed, content=None)
+                                await ctx.send(embed=embed)
 
-                                with open('userSettings.yaml', 'w') as data:
-                                    yaml.dump(userSettings, data, allow_unicode=True)
-                            elif str(dlreaction.emoji) == '👎':
+                                with open("userSettings.yaml", "w") as data:
+                                    yaml.dump(user_settings, data, allow_unicode=True)
+                            elif str(dlreaction.emoji) == "👎":
                                 await downloadmessage.delete()
-                                await message.add_reaction('⬇️')
-                        
+                                await message.add_reaction("⬇️")
+
                         else:
                             embed = discord.Embed(
-                                description=f"""{user}, this download is {round(download.filesize_approx/1000000, 2)}MB, which exceeds the maximum filesize of 100MB. It will not be processed.""")
+                                description=(
+                                    f"{user}, "
+                                    f"this download is {round(download.filesize_approx/1000000, 2)}MB, "
+                                    f"which exceeds the maximum filesize of 100MB. It will not be processed."
+                                )
+                            )
                             await downloadmessage.edit(content=None, embed=embed)
-                            
+
                 except asyncio.TimeoutError:
                     await message.clear_reactions()
                 except asyncio.CancelledError:
                     pass
-        
-        except UserCancel as e:
+
+        except UserCancel:
             await ctx.send(f"Cancelled")
-                
+
         except asyncio.TimeoutError:
             await ctx.send(f"Search timed out. Aborting")
 
         except Exception as e:
-            await ErrorHandler(bot, ctx, e, searchQuery)
-        finally: return
+            await ErrorHandler(bot, ctx, e, search_query)
+        finally:
+            return
+
 
 class UserCancel(Exception):
     pass
